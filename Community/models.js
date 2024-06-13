4dd1d10c88cbaad8cb03ba663a3028fa3b78e992
KGY <44287647+kwongiyeon@users.noreply.github.com>
2024-06-13 18:45:18 +0900

    Update models.js

--- Community/models.js
+++ Community/models.js
@@ -1,6 +1,6 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+const fs = require('fs');
+const path = require('path');
+const { fileURLToPath } = require('url');
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -99,7 +99,7 @@ const deleteComment = (commentId) => {
     }
 };
 
-export {
+module.exports = {
     getAllUsers,
     getUserByEmail,
     createUser,
